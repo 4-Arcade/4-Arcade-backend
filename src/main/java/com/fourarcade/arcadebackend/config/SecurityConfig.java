c143857bda479f24c0ebfd66177004bb126f7ab9
sungAh123 <bobaah0832@gmail.com>
2026-04-09 23:01:43 +0900

    feat: Spring Security 설정 추가

--- src/main/java/com/fourarcade/arcadebackend/config/SecurityConfig.java
+++ src/main/java/com/fourarcade/arcadebackend/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,18 +14,31 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    // BCrypt 인코더 Bean 으로 등록
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    // 필터 체인 설정
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 // REST API -> CSRF 비활성화
                 .csrf(AbstractHttpConfigurer::disable)
+
+                // 폼 로그인 및 Basic HTTP 인증 비활성화
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+
+                // 세션 정책 설정(세션을 사용하지 않음)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                // 경로별 접근 권한 설정
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/register").permitAll()  // 회원가입 열어두기
+                        .requestMatchers("/auth/register", "/auth/login").permitAll()
+
+                        // 그 외의 요청은 토큰 필요
                         .anyRequest().authenticated()
                 );
         return http.build();
