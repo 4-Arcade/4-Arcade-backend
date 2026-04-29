756c52c03a9f6aec4d5d65ecbb9cc483ae3361f9
sungAh123 <bobaah0832@gmail.com>
2026-04-29 21:27:22 +0900

    fix: room 상태 관리를 위한 redis 엔티티 확장 및 설정 갱신

--- src/main/java/com/fourarcade/arcadebackend/room/RoomRedisEntity.java
+++ src/main/java/com/fourarcade/arcadebackend/room/RoomRedisEntity.java
@@ -1,15 +1,13 @@
 package com.fourarcade.arcadebackend.room;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.io.Serializable;
 import java.util.List;
 import java.util.UUID;
 
 @Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
@@ -17,24 +15,36 @@ public class RoomRedisEntity implements Serializable {
 
     private UUID roomId;
     private String roomCode;
-    private RoomStatus status;  // "waiting", "playing", "finished"
+    private RoomStatus status;
     private UUID quizId;
     private String quizTitle;
     private RoomSettings settings;
     private List<Participant> participants;
+    private GameProgress gameProgress;
 
     public enum RoomStatus {
-        WAITING, PLAYING, FINISHED
+        WAITING, READY, IN_GAME, RESULT
     }
 
     @Getter
+    @Setter
     @Builder
     @NoArgsConstructor
     @AllArgsConstructor
     public static class Participant implements Serializable {
         private String nickname;
         private Boolean isHost;
+        private Boolean isReady;
+        private Boolean isConnected;
+        private Long disconnectedAt; // 연결 끊긴 시간
         private int score;
     }
 
+    @Getter @Setter @Builder @NoArgsConstructor @AllArgsConstructor
+    public static class GameProgress implements Serializable {
+        private int currentQuestionIndex;
+        private int totalQuestionCount;
+        private String questionStartedAt;   // ISO 8601 format
+        private int timeLimit;
+    }
 }
