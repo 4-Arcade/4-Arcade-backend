72cdce5195183aee1c40afccf0ccfc638f3460d0
sungAh123 <bobaah0832@gmail.com>
2026-05-11 15:22:16 +0900

    feat: Refresh, Logout request DTO 추가

---
+++ src/main/java/com/fourarcade/arcadebackend/auth/dto/RefreshRequest.java
@@ -0,0 +1,12 @@
+package com.fourarcade.arcadebackend.auth.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class RefreshRequest {
+    @NotBlank(message = "RefreshToken은 필수입니다.")
+    private String refreshToken;
+}
