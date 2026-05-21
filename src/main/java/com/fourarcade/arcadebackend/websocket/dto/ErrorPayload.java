1288638cf7dbc2ac493ad3e17fc13f14add87484
sungAh123 <bobaah0832@gmail.com>
2026-05-21 21:04:00 +0900

    fix: ErrorPayload message 필드 추가 및 errorCode 필드명 통일

--- src/main/java/com/fourarcade/arcadebackend/websocket/dto/ErrorPayload.java
+++ src/main/java/com/fourarcade/arcadebackend/websocket/dto/ErrorPayload.java
@@ -2,9 +2,18 @@ package com.fourarcade.arcadebackend.websocket.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class ErrorPayload {
-    private String code;  // "ROOM_NOT_FOUND", "NICKNAME_TAKEN"..
+    private String errorCode;  // "ROOM_NOT_FOUND", "NICKNAME_TAKEN"..
+    private String message;
+
+    // 기존 코드 호환용 생성자 (message 없는 경우)
+    public ErrorPayload(String errorCode) {
+        this.errorCode = errorCode;
+        this.message = null;
+    }
 }
