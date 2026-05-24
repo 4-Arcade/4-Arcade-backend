1180865bba652563153e146a6574e8e74904eee5
sungAh123 <bobaah0832@gmail.com>
2026-05-24 21:25:11 +0900

    fix: 프로필 이미지 수정 API에 multipart/form-data 명시

--- src/main/java/com/fourarcade/arcadebackend/user/controller/UserImageController.java
+++ src/main/java/com/fourarcade/arcadebackend/user/controller/UserImageController.java
@@ -5,6 +5,7 @@ import com.fourarcade.arcadebackend.common.security.CustomUserPrincipal;
 import com.fourarcade.arcadebackend.user.service.UserImageService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -21,7 +22,7 @@ public class UserImageController {
      * 유저 프로필 이미지 업로드 및 변경 API
      * POST /api/users/me/profile-image
      */
-    @PatchMapping("/me/profile-image")
+    @PatchMapping(value = "/me/profile-image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<ApiResponse<String>> updateProfileImage(
             @AuthenticationPrincipal CustomUserPrincipal principal,
             @RequestPart("image") MultipartFile file
