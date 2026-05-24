c49917a7b519ada0ab66624221f5d653957f3101
sungAh123 <bobaah0832@gmail.com>
2026-05-24 21:27:04 +0900

    docs: Swagger JWT 인증(Authorize) 설정 추가

---
+++ src/main/java/com/fourarcade/arcadebackend/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.fourarcade.arcadebackend.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        // JWT 보안 스키마 정의
+        SecurityScheme apiKey = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        // 모든 API 에 이 보안 규칙 적용한다고 선언
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("Bearer Token");
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("Bearer Token", apiKey))
+                .addSecurityItem(securityRequirement)
+                .info(new Info()
+                        .title("4-Arcade API 문서")
+                        .description("프로젝트 API 명세서")
+                        .version("v0"));
+    }
+}
